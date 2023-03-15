c59b9094de19c567f52477321128ac67e26009d9
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2023-03-15 22:35:16 +0530

    backend starts

--- PaymentBackend/settings.py
+++ PaymentBackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -73,23 +74,23 @@ WSGI_APPLICATION = 'PaymentBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'USER': 'admin',
-        'PASSWORD': 'admin123',
-        'HOST': 'database.cmxgjewbaa3s.ap-south-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'database',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin123',
+#         'HOST': 'database.cmxgjewbaa3s.ap-south-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
---
+++ api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from ant_farm.users.models import User
+
+
+# Create your models here.
+
+action_category=(
+    ('Buy','Buy'),
+    ('Sell', 'Sell')
+    )
+class User_Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,related_name='transactions')
+    date_of_transaction= models.DateTimeField(auto_now_add=True)
+    type_of_transaction= models.CharField(max_length=30, choices=type_of_user_flow, default='Subscription')
+    amount=models.IntegerField()
+        
+    
+    class Meta:
+        verbose_name_plural = "User Transactions"
+
+    def __str__(self):
+        return self.user.name + ' ' + str(self.date_of_transaction)
+
